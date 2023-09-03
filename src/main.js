371485e667b3a0fbd0294f50155a8856aabd5f4c
Nassim <jslegend@protonmail.com>
2023-09-02 22:05:31 -0400

    wrote logic to load map from Tiled

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ import gameOver from "./scenes/gameOver.js";
 import menu from "./scenes/menu.js";
 import world from "./scenes/world.js";
 
+k.loadSprite("assets", "./assets/topdownasset.png", {
+  sliceX: 39,
+  sliceY: 31,
+});
+
 const scenes = [menu, world, gameOver, endGame];
 for (const [index, scene] of scenes.entries()) {
   k.scene(index, () => scene(k));
 }
 
-k.go(0);
+k.go(1);
--- src/scenes/world.js
+++ src/scenes/world.js
@@ -1,3 +1,62 @@
-function loadMap(assetPath, tileMap, scaleFactor) {}
+export default async function world(k) {
+  console.log(k);
+  k.add([
+    k.rect(k.canvas.width, k.canvas.height),
+    k.color(76, 170, 255),
+    k.fixed(),
+  ]);
+  const mapData = await (await fetch("./assets/maps/world.json")).json();
+  const map = k.add([k.pos(0, 0), k.scale(4)]);
 
-export default function world(k) {}
+  const layers = mapData.layers;
+  for (const layer of layers) {
+    if (layer.name === "Boundaries") {
+      for (const object of layer.objects) {
+        map.add([
+          k.rect(object.width, object.height),
+          k.pos(object.x, object.y + 16),
+          k.area(),
+          k.opacity(0),
+          k.body({ isStatic: true }),
+          k.offscreen(),
+        ]);
+      }
+      continue;
+    }
+
+    let nbOfDrawnTiles = 0;
+    const tilePos = k.vec2(0, 0);
+    for (const tile of layer.data) {
+      if (nbOfDrawnTiles % layer.width === 0) {
+        tilePos.x = 0;
+        tilePos.y += mapData.tileheight;
+      } else {
+        tilePos.x += mapData.tilewidth;
+      }
+
+      nbOfDrawnTiles++;
+
+      if (tile === 0) continue;
+
+      map.add([
+        k.sprite("assets", { frame: tile - 1 }),
+        k.pos(tilePos),
+        k.offscreen(),
+      ]);
+    }
+  }
+
+  const player = k.add([
+    k.rect(16, 16),
+    k.scale(4),
+    k.area(),
+    k.body(),
+    k.pos(1000, 500),
+    { speed: 500 },
+  ]);
+  player.onUpdate(() => k.camPos(player.pos));
+  k.onKeyDown("left", () => player.move(-player.speed, 0));
+  k.onKeyDown("right", () => player.move(player.speed, 0));
+  k.onKeyDown("up", () => player.move(0, -player.speed));
+  k.onKeyDown("down", () => player.move(0, player.speed));
+}
